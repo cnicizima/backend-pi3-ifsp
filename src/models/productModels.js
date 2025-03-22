bc4f90c0ecf0ec0952a56703b0b0b8cef56f79ef
Ciro Nicizima <cironicizima@gmail.com>
2025-03-22 18:13:54 -0300

    incluido todos os models. testado todas as rotas CRUD de user

--- src/models/productModels.js
+++ src/models/productModels.js
@@ -13,3 +13,43 @@ export async function create(product) {
     })
     return result
 }
+
+export async function remove(id){
+    const result = await prisma.product.delete({
+        where: {
+            id: id
+        },
+        select: {
+            id: true,
+            name: true
+        }
+    })
+    return result
+}
+
+export async function getById(id){
+    const result = await prisma.product.findUnique({
+        where: {
+            id: id
+        },
+        select: {
+            id: true,
+            name: true
+        }
+    })
+    return result
+}
+
+export async function update( id, product) {
+    const result = await prisma.product.update({
+        where: {
+            id: id
+        },
+        data: product,
+        select:{
+            id: true,
+            name: true
+        }
+    })
+    return result
+}
--- src/server.js
+++ src/server.js
@@ -24,6 +24,6 @@ app.use('/products', productRouter);
 
 
 
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
+app.listen(8000, () => {
+  console.log('Server is running on http://localhost:8000');
 });
