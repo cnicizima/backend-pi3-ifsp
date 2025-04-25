f3f402e3eb274d14980c38656384ebdf2d498583
cironicizima <c.nicizima@aluno.ifsp.edu.br>
2025-04-25 15:30:47 -0300

    correçao do banco

--- src/controllers/Users/createUserController.js
+++ src/controllers/Users/createUserController.js
@@ -1,27 +1,21 @@
-import { create, userValidator } from '../../models/userModels.js';
+import { create } from '../../models/userModels.js';
 
 export default async function createUserController(req, res){
-    const validation = userValidator(req.body);
-    
-    if (!validation.success) {
-        return res.status(400).json({
-            message: 'Erro de validação',
-            errors: validation.error.format()
-        });
-    }
+    const user = req.body;
 
-    const result = await create(validation.data);
+    const result = await create(user);
 
     if(!result){
         return res.status(500).json({ 
-            message: 'Erro ao criar usuário'
-        });
-    }
-
-    delete result.password;
-
-    return res.status(201).json({ 
+          message: 'Erro ao criar usuário'
+        })
+      }
+  
+      delete result.pass //para nao enviar a senha no json de retorno
+  
+      return res.status(201).json({ 
         message: 'Usuário criado com sucesso',
         user: result    
-    });
-}
+      })
+    }
+  
